bb2a44b34215f831d2f8d540b1f3d3b123cba590
guillermo espinoza cortes <utp0001443@alumno.utpuebla.edu.mx>
2020-07-08 21:32:31 -0500

    Modulos final ;v

--- MOCos_V1/Controllers/MaestroController.cs
+++ MOCos_V1/Controllers/MaestroController.cs
@@ -21,5 +21,12 @@ namespace MOCos_V1.Controllers
 
             return View();
         }
+
+        public ActionResult Temas()
+        {
+            ViewBag.Message = "Your Materias.";
+
+            return View();
+        }
     }
 }
