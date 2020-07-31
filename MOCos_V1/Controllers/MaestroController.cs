53aee0489cc9a06c5f3dd5a2d09be7cd193b1a46
guillermo espinoza cortes <utp0001443@alumno.utpuebla.edu.mx>
2020-07-30 20:15:41 -0500

    Cambios bien chidoris la dvd ;v

--- MOCos_V1/Controllers/MaestroController.cs
+++ MOCos_V1/Controllers/MaestroController.cs
@@ -33,17 +33,17 @@ namespace MOCos_V1.Controllers
         {
             try
             {
-                
+
                 using (mocOS_BDEntities bd = new mocOS_BDEntities())
                 {
                     Usuario obj = new Usuario();
                     obj = (Usuario)Session["User"];
                     var prof = (from d in bd.Profesor
-                                 where d.idUsuario == obj.idUsuario
-                                 select d).FirstOrDefault();
+                                where d.idUsuario == obj.idUsuario
+                                select d).FirstOrDefault();
                     var mat = (from d in bd.Materia
-                                   where d.idCoordinador == prof.idProfesor
-                                   select d).FirstOrDefault();
+                               where d.idCoordinador == prof.idProfesor
+                               select d).FirstOrDefault();
                     Session["UserMateria"] = mat;
                     ViewBag.Materia = mat.NombreMateria;
                     List<Unidad> Uni = bd.Unidad.Where(x => x.idMateria == mat.idMateria).ToList();
@@ -58,17 +58,37 @@ namespace MOCos_V1.Controllers
         }
 
         [AuthorizeUser(idNivel: 4)]
+        [HttpGet]
+        public ActionResult InsertarUnidad()
+        {
+            Materia mat = (Materia)Session["UserMateria"];
+            ViewBag.Materia = mat.NombreMateria;
+            return View();
+        }
+
+        [AuthorizeUser(idNivel: 4)]
         [HttpPost]
-        public ActionResult Insertar_Unidad(Unidad obj)
+        public ActionResult InsertarUnidad(Unidad obj)
         {
             try
             {
-                using (mocOS_BDEntities bd = new mocOS_BDEntities())
+                if (obj.nombre != null)
                 {
-                    obj.idMateria = (int)Session["UserMateria"];
-                    bd.Unidad.Add(obj);
-                    bd.SaveChanges();
-                    return RedirectToAction("Modulos");
+                    using (mocOS_BDEntities bd = new mocOS_BDEntities())
+                    {
+                        Materia mat = (Materia)Session["UserMateria"];
+                        ViewBag.Materia = mat.NombreMateria;
+                        obj.idMateria = mat.idMateria;
+                        bd.Unidad.Add(obj);
+                        bd.SaveChanges();
+                        return RedirectToAction("MostrarModulos");
+                    }
+                }
+                else
+                {
+                    Materia mat = (Materia)Session["UserMateria"];
+                    ViewBag.Materia = mat.NombreMateria;
+                    return View();
                 }
             }
             catch (Exception mensaje)
@@ -77,12 +97,6 @@ namespace MOCos_V1.Controllers
                 return View();
             }
         }
-
-        public ActionResult InsertarUnidad()
-        {
-            ViewBag.Message = "New Unit";
-
-            return View();
-        }
+        
     }
 }
